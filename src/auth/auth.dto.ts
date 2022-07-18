00dc568249e77e26bfe8227326a218387b27346d
Song-YunMin <thd930308@naver.com>
2022-07-19 02:09:53 +0900

    [UPDATE] 카카오 유저 정보 인터페이스 추가

---
+++ src/auth/auth.dto.ts
@@ -0,0 +1,29 @@
+export interface IKakaoUserInformation {
+  id: number,
+  connected_at: string,
+  properties: {
+    nickname: string,
+    profile_image: string,
+    thumbnail_image: string
+  },
+  kakao_account: {
+    profile_nickname_needs_agreement: boolean,
+    profile_image_needs_agreement: boolean,
+    profile: object,
+    has_email: boolean,
+    email_needs_agreement: boolean,
+    is_email_valid: boolean,
+    is_email_verified: boolean,
+    email: string,
+    has_age_range: boolean,
+    age_range_needs_agreement: boolean,
+    age_range: string,
+    has_birthday: boolean,
+    birthday_needs_agreement: boolean,
+    birthday: string,
+    birthday_type: string,
+    has_gender: boolean,
+    gender_needs_agreement: boolean,
+    gender: string
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom, map } from 'rxjs';
+import { IKakaoUserInformation } from './auth.dto';
+
 const REST_API_KEY = process.env.DEV_KAKAO_REST_API_KEY;
 const REDIRECT_URI = process.env.DEV_KAKAO_REDIRECT_URI;
 
@@ -8,33 +10,29 @@ const REDIRECT_URI = process.env.DEV_KAKAO_REDIRECT_URI;
 export class AuthService {
   constructor(private readonly httpService: HttpService) {
   }
+
   authTest(): string {
-    return 'Auth Test'
+    return 'Auth Test';
   }
 
   // TODO: 사용하지 않을 예정
   public async getAuthToken(): Promise<object> {
     return await firstValueFrom(this.httpService.get(`https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`, {
-      headers: {'Content-Type': 'x-www-form-urlencoded'}
-    }).pipe(map(response => [response.data, response.status]),));
+      headers: { 'Content-Type': 'x-www-form-urlencoded' },
+    }).pipe(map(response => [response.data, response.status])));
   }
 
   public async kakaoLogin(query: string): Promise<any> {
-    return 'ok';
+    return 'test';
   }
 
-  public async getKakaoUserInformation(accessToken: string): Promise<any> {
-    const response: object = await firstValueFrom(this.httpService.get('https://kapi.kakao.com/v2/user/me', {
-      method: "GET",
-      headers: { "Authorization": `Bearer ${accessToken}` }
-    }).pipe(map(response => [response.data, response.status])));
-    const NETWORK_SUCCESS_STATUS_CODE = 200;
+  public async getKakaoUserInformation(accessToken: string): Promise<IKakaoUserInformation> {
     try {
-      if (response[1] === NETWORK_SUCCESS_STATUS_CODE) {
-        return response;
-      } else {
-        return "kakao login openAPI error: " + response["statusText"];
-      }
+      const response: object = await firstValueFrom(this.httpService.get('https://kapi.kakao.com/v2/user/me', {
+        method: 'GET',
+        headers: { 'Authorization': `Bearer ${accessToken}` },
+      }).pipe(map(response => [response.data, response.status])));
+      return response[0];
     } catch (e) {
       throw new Error(e);
     }
