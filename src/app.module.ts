4206301ed16a10d2dbe61a2716c0c65d436c406b
fuzzysound <hyun1652@gmail.com>
2022-08-09 22:15:01 +0900

    Remove logging for test

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,6 @@ const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 const MONGO_HOST = process.env.MONGO_HOST;
 const MONGO_PORT = process.env.MONGO_PORT;
 const MONGO_DB = process.env.MONGO_DB;
-const MONGO = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`;
-console.log(MONGO);
 
 @Module({
   imports: [
