c4d3af58ec6e2b9c2d6a9900aa7775b04976e41a
fuzzysound <hyun1652@gmail.com>
2022-08-09 22:23:51 +0900

    Optimize imports

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, Query } from 'mongoose';
-import { User, UserDocument, UserSchema } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
 export class UserRepository {
