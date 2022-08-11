7b1277f832e7f122e97bf89496612533f13adf8e
fuzzysound <hyun1652@gmail.com>
2022-08-12 00:49:36 +0900

    Apply Swagger documentation to user module

--- src/user/dtos/user.dto.ts
+++ src/user/dtos/user.dto.ts
@@ -1,11 +1,27 @@
 import { User, UserUpdate } from './user.interface';
 import { User as UserDoc } from '../schemas/user.schema';
 import { PERSONALITY } from '../schemas/personality';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserDTO implements User {
+  @ApiProperty({
+    description: '유저 ID',
+  })
   id: string;
+
+  @ApiProperty({
+    description: '유저 닉네임',
+  })
   nickname: string;
+
+  @ApiProperty({
+    description: '프로필 이미지 경로',
+  })
   profileImagePath: string;
+
+  @ApiProperty({
+    description: '유저 성향',
+  })
   personality: string;
 
   static from(user: UserDoc): UserDTO {
@@ -26,7 +42,16 @@ export class UserDTO implements User {
 }
 
 export class UserUpdateDTO implements UserUpdate {
+  @ApiProperty({
+    description: '유저 닉네임',
+    required: false,
+  })
   nickname: string;
+
+  @ApiProperty({
+    description: '프로필 이미지 경로',
+    required: false,
+  })
   profileImagePath: string;
 
   constructor(nickname: string, profileImagePath: string) {
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,11 @@
 import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
+import {
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserDTO, UserUpdateDTO } from './dtos/user.dto';
 import { UserService } from './user.service';
 
@@ -6,11 +13,26 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiTags('User')
+  @ApiOperation({
+    summary: '유저 정보 조회',
+    description: '유저 ID로 유저 정보를 조회한다.',
+  })
+  @ApiOkResponse({ description: '성공', type: UserDTO })
+  @ApiNotFoundResponse({ description: '유저를 찾을 수 없음' })
+  @ApiInternalServerErrorResponse({ description: '서버 오류' })
   @Get(':id')
   public async getUser(@Param('id') id: string): Promise<UserDTO> {
     return await this.userService.getUser(id);
   }
 
+  @ApiTags('User')
+  @ApiOperation({
+    summary: '유저 정보 수정',
+    description: '유저 닉네임 혹은 프로필 사진을 수정한다.',
+  })
+  @ApiOkResponse({ description: '성공', type: UserDTO })
+  @ApiInternalServerErrorResponse({ description: '서버 오류' })
   @Patch(':id')
   public async updateUser(
     @Param('id') id: string,
