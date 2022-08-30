066d300b0e10c3c8dcdfd80ca8b7ac784d6fdd83
fuzzysound <hyun1652@gmail.com>
2022-08-31 01:14:16 +0900

    스웨거 annotation 추가

--- src/journey/dtos/journey.dto.ts
+++ src/journey/dtos/journey.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   ArrayMaxSize,
@@ -22,22 +23,44 @@ import {
 import { ORIENTATION } from '../schemas/orientation';
 import { IdResponse, JourneyCreate, TagCreate } from './journey.interface';
 
+export class TagCreateDTO implements TagCreate {
+  @ApiProperty({ description: '태그 주제' })
+  @MaxLength(MAX_TAG_TOPIC_LENGTH, { message: TAG_TOPIC_LENGTH_EXCEEDED_MSG })
+  @IsNotEmpty({ message: IS_NOT_EMPTY_KIND_MSG('태그 이름') })
+  readonly topic: string;
+
+  @ApiProperty({ description: '태그 성향' })
+  @IsIn(Object.values(ORIENTATION), { message: IS_IN_MSG })
+  @IsNotEmpty({ message: IS_NOT_EMPTY_MSG })
+  readonly orientation: string;
+
+  constructor(topic: string, orientation: string) {
+    this.topic = topic;
+    this.orientation = orientation;
+  }
+}
+
 export class JourneyCreateDTO implements JourneyCreate {
+  @ApiProperty({ description: '여행 이름' })
   @MaxLength(MAX_JOURNEY_NAME_LENGTH, {
     message: JOURNEY_NAME_LENGTH_EXCEEDED_MSG,
   })
   @IsNotEmpty({ message: IS_NOT_EMPTY_KIND_MSG('여행 이름') })
   readonly name: string;
 
+  @ApiProperty({ description: '여행 시작일 timestamp (초단위, 10자리)' })
   @IsNotEmpty({ message: IS_NOT_EMPTY_KIND_MSG('여행 시작일') })
   readonly startDate: number;
 
+  @ApiProperty({ description: '여행 종료일 timestamp (초단위, 10자리)' })
   @IsNotEmpty({ message: IS_NOT_EMPTY_KIND_MSG('여행 종료일') })
   readonly endDate: number;
 
+  @ApiProperty({ description: '여행기 커버 이미지 경로' })
   @IsNotEmpty({ message: IS_NOT_EMPTY_MSG })
   readonly themePath: string;
 
+  @ApiProperty({ type: [TagCreateDTO], description: '태그 목록' })
   @ArrayMaxSize(MAX_TAGS, { message: TAG_EXCEEDED_MSG })
   @ValidateNested({ each: true })
   @Type(() => TagCreateDTO)
@@ -58,22 +81,8 @@ export class JourneyCreateDTO implements JourneyCreate {
   }
 }
 
-export class TagCreateDTO implements TagCreate {
-  @MaxLength(MAX_TAG_TOPIC_LENGTH, { message: TAG_TOPIC_LENGTH_EXCEEDED_MSG })
-  @IsNotEmpty({ message: IS_NOT_EMPTY_KIND_MSG('태그 이름') })
-  readonly topic: string;
-
-  @IsIn(Object.values(ORIENTATION), { message: IS_IN_MSG })
-  @IsNotEmpty({ message: IS_NOT_EMPTY_MSG })
-  readonly orientation: string;
-
-  constructor(topic: string, orientation: string) {
-    this.topic = topic;
-    this.orientation = orientation;
-  }
-}
-
 export class IdResponseDTO implements IdResponse {
+  @ApiProperty({ description: '변동된 다큐먼트의 ID' })
   readonly id: string;
 
   constructor(id: string) {
--- src/journey/journey.controller.ts
+++ src/journey/journey.controller.ts
@@ -7,6 +7,14 @@ import {
   UsePipes,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { TimeRangeValidationPipe } from '../common/pipe/custom.validation.pipe';
 import { JourneyCreateDTO, IdResponseDTO } from './dtos/journey.dto';
 import { JourneyService } from './journey.service';
@@ -15,6 +23,15 @@ import { JourneyService } from './journey.service';
 export class JourneyController {
   constructor(private readonly journeyService: JourneyService) {}
 
+  @ApiTags('Journey')
+  @ApiOperation({
+    summary: '여행 생성',
+    description: '새로운 여행을 생성한다.',
+  })
+  @ApiCreatedResponse({ description: '성공', type: IdResponseDTO })
+  @ApiBadRequestResponse({ description: '요청 데이터가 잘못됨' })
+  @ApiInternalServerErrorResponse({ description: '서버 오류' })
+  @ApiBearerAuth()
   @UseGuards(AuthGuard('jwt'))
   @UsePipes(new TimeRangeValidationPipe())
   @Post()
