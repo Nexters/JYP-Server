f24e174055653f051b0a591e54097b0797728b97
fuzzysound <hyun1652@gmail.com>
2022-09-02 23:36:47 +0900

    UserService 테스트 코드 간략화

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -15,18 +15,14 @@ const AUTH_ID = 'id';
 const ID = generateId(AUTH_VENDOR, AUTH_ID);
 const NAME = 'name';
 const IMG = '/image/path';
-const PSN = PERSONALITY.ME;
-const user = new User();
+const PSN_ID = 'ME';
+const USER = new User(ID, NAME, IMG, PSN_ID);
 
 describe('UserService', () => {
   let userRepository: UserRepository;
   let userService: UserService;
 
   beforeEach(async () => {
-    user._id = ID;
-    user.name = NAME;
-    user.img = IMG;
-    user.psn = PSN;
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserService, UserRepository],
     })
@@ -46,7 +42,7 @@ describe('UserService', () => {
     // given
     const findOne = On(userRepository)
       .get(method(() => userRepository.findOne))
-      .mockResolvedValue(user);
+      .mockResolvedValue(USER);
 
     // when
     const result = await userService.getUser(ID);
@@ -55,10 +51,10 @@ describe('UserService', () => {
     expect(findOne).toBeCalledTimes(1);
     expect(findOne).toBeCalledWith(ID);
     const optionContent = result.getOrUndefined();
-    expect(optionContent.id).toBe(user._id);
-    expect(optionContent.name).toBe(user.name);
-    expect(optionContent.profileImagePath).toBe(user.img);
-    expect(optionContent.personality).toBe(PERSONALITY[user.psn]);
+    expect(optionContent.id).toBe(USER._id);
+    expect(optionContent.name).toBe(USER.name);
+    expect(optionContent.profileImagePath).toBe(USER.img);
+    expect(optionContent.personality).toBe(PERSONALITY[USER.psn]);
   });
 
   it('UserRepository.findOne이 null을 리턴할 경우 getUser는 None을 리턴한다', async () => {
@@ -80,7 +76,7 @@ describe('UserService', () => {
     // given
     const updateOne = On(userRepository)
       .get(method(() => userRepository.updateOne))
-      .mockResolvedValue(user);
+      .mockResolvedValue(USER);
 
     // when
     const userUpdateDTO = new UserUpdateDTO(NAME, IMG);
@@ -89,17 +85,17 @@ describe('UserService', () => {
     // then
     expect(updateOne).toBeCalledTimes(1);
     expect(updateOne).toBeCalledWith(ID, NAME, IMG);
-    expect(result.id).toBe(user._id);
-    expect(result.name).toBe(user.name);
-    expect(result.profileImagePath).toBe(user.img);
-    expect(result.personality).toBe(PERSONALITY[user.psn]);
+    expect(result.id).toBe(USER._id);
+    expect(result.name).toBe(USER.name);
+    expect(result.profileImagePath).toBe(USER.img);
+    expect(result.personality).toBe(PERSONALITY[USER.psn]);
   });
 
   it('createUser는 UserRepository.insertOne을 호출한다', async () => {
     // given
     const insertOne = On(userRepository)
       .get(method(() => userRepository.insertOne))
-      .mockResolvedValue(user);
+      .mockResolvedValue(USER);
 
     // when
     const userCreateDTO = new UserCreateDTO(
@@ -107,16 +103,16 @@ describe('UserService', () => {
       AUTH_ID,
       NAME,
       IMG,
-      PSN,
+      PSN_ID,
     );
     const result = await userService.createUser(userCreateDTO);
 
     // then
     expect(insertOne).toBeCalledTimes(1);
-    expect(insertOne).toBeCalledWith(ID, NAME, IMG, PSN);
-    expect(result.id).toBe(user._id);
-    expect(result.name).toBe(user.name);
-    expect(result.profileImagePath).toBe(user.img);
-    expect(result.personality).toBe(PERSONALITY[user.psn]);
+    expect(insertOne).toBeCalledWith(ID, NAME, IMG, PSN_ID);
+    expect(result.id).toBe(USER._id);
+    expect(result.name).toBe(USER.name);
+    expect(result.profileImagePath).toBe(USER.img);
+    expect(result.personality).toBe(PERSONALITY[USER.psn]);
   });
 });
