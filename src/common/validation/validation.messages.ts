406bdb3e712e77868981338ef1ef57c6752d0db9
fuzzysound <hyun1652@gmail.com>
2022-11-05 22:45:29 +0900

    Validation 메시지 변경

--- src/common/validation/validation.messages.ts
+++ src/common/validation/validation.messages.ts
@@ -11,6 +11,7 @@ import {
   MAX_TAG_TOPIC_LENGTH,
   MIN_LATITUDE,
   MIN_LONGITUDE,
+  MAX_USER_PER_JOURNEY,
 } from './validation.constants';
 
 export const MAX_LENGTH_MSG =
@@ -26,24 +27,24 @@ export const IS_EMUN_MSG = (args: ValidationArguments) => {
 
 export const IS_IN_MSG = '$property 값은 $constraint1 중 하나여야 합니다.';
 
-export const JOURNEY_EXCEEDED_MSG = `여행은 ${MAX_JOURNEY_PER_USER}개까지만 만들 수 있어요!`;
+export const JOURNEY_EXCEEDED_MSG = `여행 플래너는 ${MAX_JOURNEY_PER_USER}개까지 만들 수 있어요`;
 
-export const TIME_RANGE_INVALID_MSG = `여행 기간은 ${MAX_JOURNEY_DAYS}일 이내로 설정해주세요!`;
+export const TIME_RANGE_INVALID_MSG = `여행 기간은 ${MAX_JOURNEY_DAYS}일 이내까지 가능해요`;
 
-export const TAG_EXCEEDED_MSG = `태그는 ${MAX_TAGS}개까지만 설정할 수 있어요!`;
+export const TAG_EXCEEDED_MSG = `태그 ${MAX_TAGS}개까지 만들 수 있어요`;
 
-export const JOURNEY_NAME_LENGTH_EXCEEDED_MSG = `여행 이름은 ${MAX_JOURNEY_NAME_LENGTH}자 이내로 입력해주세요!`;
+export const JOURNEY_NAME_LENGTH_EXCEEDED_MSG = `최대 ${MAX_JOURNEY_NAME_LENGTH}글자 이내로 입력해주세요`;
 
-export const TAG_TOPIC_LENGTH_EXCEEDED_MSG = `태그 이름은 ${MAX_TAG_TOPIC_LENGTH}자 이내로 입력해주세요!`;
+export const TAG_TOPIC_LENGTH_EXCEEDED_MSG = `${MAX_TAG_TOPIC_LENGTH}글자까지 입력 가능해요`;
 
 export const IS_NOT_EMPTY_KIND_MSG = (field: string) =>
   `${field} 정보가 입력되지 않았어요!`;
 
-export const INVALID_ID_IN_JWT_MSG = `존재하지 않는 회원 ID로 인증되었습니다.`;
+export const INVALID_ID_IN_JWT_MSG = `존재하지 않는 회원 ID에요`;
 
-export const JOURNEY_NOT_EXIST_MSG = `해당 여행이 존재하지 않습니다.`;
+export const JOURNEY_NOT_EXIST_MSG = `아쉽지만 다음에 함께해요`;
 
-export const PIKMI_EXCEEDED_MSG = `여행 후보 장소는 ${MAX_PIKMI_PER_JOURNEY}개까지만 추가할 수 있어요!`;
+export const PIKMI_EXCEEDED_MSG = `후보 장소는 최대 ${MAX_PIKMI_PER_JOURNEY}개까지 가능해요!`;
 
 export const LONGITUDE_INVALID_MSG = `경도 값은 ${MIN_LONGITUDE}~${MAX_LONGITUDE} 사이의 값이어야 해요!`;
 
@@ -59,7 +60,7 @@ export const PIKI_EXCEEDED_MSG = `여행 장소는 최대 ${MAX_PIKI_PER_DAY}개
 
 export const USER_NOT_IN_JOURNEY_MSG = `여행에 소속되지 않은 회원의 요청이에요!`;
 
-export const USER_EXCEEDED_MSG = `정원이 차서 여행에 참여할 수 없어요!`;
+export const USER_EXCEEDED_MSG = `최대 ${MAX_USER_PER_JOURNEY}명까지 입장할 수 있어요`;
 
 export const PIKMI_NOT_EXIST_MSG = `해당 여행 후보 장소가 존재하지 않아요!`;
 
