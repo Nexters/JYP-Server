354d639b394ba0034edbc0662ae717181dce8c61
Song-YunMin <thd930308@naver.com>
2023-03-06 01:17:27 +0900

    logging middleware 생성

---
+++ src/common/logging/logging.middleware.ts
@@ -0,0 +1,12 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import * as morgan from 'morgan';
+
+@Injectable()
+export class LoggingMiddleware implements NestMiddleware {
+  private logger = morgan('combined');
+
+  use(req: Request, res: Response, next: NextFunction) {
+    this.logger(req, res, next);
+  }
+}
